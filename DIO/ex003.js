477c54772fa5e006b8850308a2984546224cdfc0
ERIK BARBOSA DA COSTA <79406452+atsireopac@users.noreply.github.com>
2024-04-26 15:39:52 -0300

    atualização de exercicio

---
+++ DIO/ex003.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 10; i++) {
+  console.log(i + ` x 1 = `+ i*1)
+  console.log(i + ` x 2 = `+ i*2)
+  console.log(i + ` x 3 = `+ i*3)
+  console.log(i + ` x 4 = `+ i*4)
+  console.log(i + ` x 5 = `+ i*5)
+  console.log(i + ` x 6 = `+ i*6)
+  console.log(i + ` x 7 = `+ i*7)
+  console.log(i + ` x 8 = `+ i*8)
+  console.log(i + ` x 9 = `+ i*9)
+  console.log(i + ` x 10 = `+ i*10)
+
+}
